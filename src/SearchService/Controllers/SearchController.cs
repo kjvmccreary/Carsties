01689932fa02fab9c2f956a00913c764d2dfa173
kevinmccreary <kevin@smartgroup-us.com>
2024-02-06 16:59:09 -0600

    Update SearchItems to handle additional search parameters
    The SearchItems function in SearchController has been updated to accept additional parameters such as orderBy and filterBy. These improvements allow for sorting and filtering on the basis of make, creation date, auction end time, seller, or winner. The code also supports receiving these parameters in the query, increasing the flexibility of search operations.

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -11,19 +11,43 @@ public class SearchController : ControllerBase
 {
     // GET
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems([FromBody] SearchParams searchParams)
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
     {
-        var query = DB.PagedSearch<Item>();
+        var query = DB.PagedSearch<Item,Item>();
 
-        query.Sort(c => c.Ascending(a => a.Make));
+        // query.Sort(c => c.Ascending(a => a.Make));
 
         if (!string.IsNullOrEmpty(searchParams.SearchTerm))
         {
             query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
         }
 
-        query.PageNumber(pageNumber);
-        query.PageSize(pageSize);
+        query = searchParams.OrderBy switch
+        {
+            "make" => query.Sort(c => c.Ascending(d => d.Make)),
+            "new" => query.Sort(c => c.Descending(d => d.CreatedAt)),
+            _ => query.Sort(c => c.Ascending(d => d.AuctionEnd))
+        };
+        query = searchParams.FilterBy switch
+        {
+            "finished" => query.Match(c => c.AuctionEnd < DateTime.UtcNow),
+            "endingSoon" => query.Match(c => c.AuctionEnd < DateTime.UtcNow.AddHours(6)
+                                             && c.AuctionEnd > DateTime.UtcNow),
+            _ => query.Match(c => c.AuctionEnd > DateTime.UtcNow)
+        };
+
+        if (!string.IsNullOrEmpty(searchParams.Seller))
+        {
+            query.Match(c => c.Seller == searchParams.Seller);
+        }
+
+        if (!string.IsNullOrEmpty(searchParams.Winner))
+        {
+            query.Match(c => c.Winner == searchParams.Winner);
+        }
+
+        query.PageNumber(searchParams.PageNumber);
+        query.PageSize(searchParams.PageSize);
         
         var result = await query.ExecuteAsync();
 
